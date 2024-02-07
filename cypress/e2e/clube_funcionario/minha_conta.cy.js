cb4429d9ad3fb6adeceac3ac7b9a7a83175ee41c
cinthiabs <cinthiabarbosa3d@gmail.com>
2024-02-07 18:31:13 -0300

    minha_conta

--- cypress/e2e/clube_funcionario/minha_conta.cy.js
+++ cypress/e2e/clube_funcionario/minha_conta.cy.js
@@ -1,8 +1,47 @@
 /// <reference types="cypress" />
+import faker from 'faker';
 
-before(()=>{
+//Variaveis 
+const botaoSalvar = '.btn'
+
+beforeEach(()=>{
     cy.AcessarSistemaFuncionario()
 });
-after(()=>{
-    cy.LogoutSistemaFuncionario()
+afterEach(()=>{
+   cy.LogoutSistemaFuncionario()
+})
+
+
+describe('Regressivo - Minha conta', () => {
+    it('Positivo - Alteração de dados',() => {
+        cy.get('#minha-conta').click()
+        cy.get('h4').should('be.visible','Minha Conta')
+        cy.get('[name="nome"]').clear().type(faker.name.findName())
+        cy.get('.celular').clear().type(faker.phone.phoneNumberFormat())
+        
+        //valida cep
+        cy.get('#cep').clear().type('09371100')
+        cy.get('#numero').type('1') 
+        cy.get('#endereco').invoke('val').should('not.be.empty');
+        cy.get('#cidade').invoke('val').should('not.be.empty');
+        cy.get('#tipo').invoke('val').should('not.be.empty');
+        cy.get('#bairro').invoke('val').should('not.be.empty');
+        cy.get('#estado').invoke('val').should('not.be.empty');
+        cy.get('.btn').click()
+
+    })
+    it('Negativo - Alteração de dados',() => {
+        cy.get('#minha-conta').click()
+        cy.get('h4').should('be.visible','Minha Conta')
+        cy.get('[name="nome"]').clear().type(faker.name.findName())
+        cy.get('.celular').clear().type(faker.lorem.paragraph())
+        cy.get('[type="email"]').clear().type(faker.lorem.paragraph()) 
+        // cep invalido
+        cy.get('#cep').clear().type('00000000')
+        cy.get('#endereco').click()
+        cy.wait(1000)
+        cy.get('#endereco').invoke('val').should('be.empty');
+        cy.get('#cidade').invoke('val').should('be.empty');
+        cy.get('#bairro').invoke('val').should('be.empty');
+    })
 })
--- cypress/support/logout.js
+++ cypress/support/logout.js
@@ -1,4 +1,5 @@
 Cypress.Commands.add("LogoutSistemaFuncionario",() =>{
+    cy.window().scrollTo('top');
     cy.get('.perfil-clean > a > .bi').click()
     cy.get('h3.text-center').should('contain','Entre no Sistema')
 });
