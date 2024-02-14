431521ebb438c5f871b5a502979dda683c095dae
cinthiabs <cinthiabarbosa3d@gmail.com>
2024-02-14 19:16:03 -0300

    funcionadade arquivo, valida alert

--- cypress/e2e/clube_funcionario/minha_conta.cy.js
+++ cypress/e2e/clube_funcionario/minha_conta.cy.js
@@ -1,16 +1,12 @@
 /// <reference types="cypress" />
 import faker from 'faker';
 
-//Variaveis 
-const botaoSalvar = '.btn'
-
 beforeEach(()=>{
     cy.AcessarSistemaFuncionario()
 });
 afterEach(()=>{
    cy.LogoutSistemaFuncionario()
 })
-
 
 describe('Regressivo - Minha conta', () => {
     it('Positivo - Alteração de dados',() => {
@@ -29,19 +25,37 @@ describe('Regressivo - Minha conta', () => {
         cy.get('#estado').invoke('val').should('not.be.empty');
         cy.get('.btn').click()
 
-    })
-    it('Negativo - Alteração de dados',() => {
+    });
+    it.only('Negativo - Alteração de dados',() => {
         cy.get('#minha-conta').click()
         cy.get('h4').should('be.visible','Minha Conta')
         cy.get('[name="nome"]').clear().type(faker.name.findName())
         cy.get('.celular').clear().type(faker.lorem.paragraph())
         cy.get('[type="email"]').clear().type(faker.lorem.paragraph()) 
+        
         // cep invalido
         cy.get('#cep').clear().type('00000000')
         cy.get('#endereco').click()
         cy.wait(1000)
+        cy.on('window:alert', (message) => {
+            expect(message).to.equal('CEP não encontrado.');
+        });
         cy.get('#endereco').invoke('val').should('be.empty');
         cy.get('#cidade').invoke('val').should('be.empty');
         cy.get('#bairro').invoke('val').should('be.empty');
-    })
-})
+        
+    });
+    it('Funcionalidade - Envie uma foto sua',() => {
+        const fileInputSelector = ':nth-child(2) > .form-control';
+        const fileName = 'Teste.png';
+        const filePath = `./cypress/fixtures/${fileName}`;
+
+        cy.get('#minha-conta').click()
+        cy.get('h4').should('be.visible','Minha Conta')
+
+        // Faz o upload do arquivo
+        cy.uploadFile(fileInputSelector, fileName, filePath);
+        cy.get('.btn').click()
+        cy.get('.succWrap').should('contain','Dados atualizados!')
+    });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,21 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import 'cypress-file-upload';
+
+Cypress.Commands.add('uploadFile', (selector, fileName, fileType = ' ', force = false) => {
+  cy.get(selector).then((subject) => {
+    cy.fixture(fileName, 'base64')
+      .then(Cypress.Blob.base64StringToBlob)
+      .then((blob) => {
+        const el = subject[0];
+        const testFile = new File([blob], fileName, { type: fileType });
+        const dataTransfer = new DataTransfer();
+
+        dataTransfer.items.add(testFile);
+        el.files = dataTransfer.files;
+
+        cy.wrap(subject).trigger('change', { force });
+      });
+  });
+});
