d1d3e3261cc7fdfc6144c29c770ce86cf9615751
cinthiabs <cinthiabarbosa8d@outlook.com>
2024-06-15 08:47:10 -0300

    revert

--- cypress/e2e/employee/agendamentos.cy.js
+++ cypress/e2e/employee/agendamentos.cy.js
@@ -1,23 +1,23 @@
 /// <reference types="cypress" />
 
+
 beforeEach(()=>{
     cy.LoginEmployee()
     cy.get('#meus-agendamentos').click()
         cy.get('h4').should('be.visible','Agendamentos')
 });
 afterEach(()=>{
-  // cy.Logout()
+   cy.Logout()
 });
 
-describe('Schedules', () => {
-    it('New schedule, view and delete schedule',() => {
+describe('Regressivo - Agendamentos', () => {
+    it.skip('Positivo - Novo agendamento',() => {
         cy.get('.col-sm-3 > .btn').should('contain','Novo Agendamento').click()
         
-        cy.get('.confraternizacao > h2').should('be.visible','Espaços de Confraternização')
         cy.get('.confraternizacao')
         .contains('Espaço Gourmet')
         .parent(':nth-child(3)') 
         .find('.btnlugares > .green')
-        .click()
+        .click();
     });
 });
